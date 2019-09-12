31a929b4b6933c35f2bc995bb3b7bab4450abd61
shadslattery <shadslattery@gmail.com>
2019-09-12 13:09:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/colorcolumns/colorcolumns.java
@@ -0,0 +1,19 @@
+package com.example.colorcolumns;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+
+public class colorcolumns extends AppCompatActivity {
+
+@Override
+
+    protected void onCreate(@Nullable Bundle saveInstanceState)
+
+{
+    super.onCreate(saveInstanceState);
+    setContentView(R.layout.colorlocated);
+
+    }
+}
